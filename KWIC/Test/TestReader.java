0a1561a060dfd568b9d240cd60d87418dc347ac1
meike <meike@r-20-177-28-172.comp.nus.edu.sg>
2014-08-25 11:35:38 +0800

    Added a test package

---
+++ KWIC/Test/TestReader.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 
+ */
+
+
+/**
+ * @author Meike Aichele - A1028157A
+ *
+ */
+public class TestReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length > 0)
+		try {
+			SimpleFileReader fr = new SimpleFileReader(args[0]);
+			System.out.println(fr.fileContent);
+			SimpleFileWriter fw = new SimpleFileWriter(fr.fileContent.split(System.lineSeparator()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
